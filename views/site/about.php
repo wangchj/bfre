62e516fce481b9f799f58778f7a1bcfe20ef0c6b
wangchj <cjw39@hotmail.com>
2014-12-10 23:49:15 -0600

    Add about page.

--- views/site/about.php
+++ views/site/about.php
@@ -2,15 +2,33 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-$this->title = 'About';
+$this->title = 'About Us';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p>
+    <div class="row">
+    <div class="col-sm-8">
+        <p>
+        Bill Fowler Real Estate operates in the Southeaster United Sates and specializes
+        in Land, Commercial, and 1031 Tax-Differed Exchanges. With more than four decades
+        in the business, the company has experienced the full spectrum of issues involved
+        in every aspect of real estate transactions, some of which were extremely complicated.
+        </p>
 
-    <code><?= __FILE__ ?></code>
+        <p>
+        Our company has been successful in transactions that involved numerous
+        entities with diverse goals. Succeeding where other have been unable to craft a
+        deal that satisfies everyone continues to be a hallmark of the company.
+        </p>
+
+        <p>
+        We provide personal care with absolute integrity; and as such is the go-to
+        organization for both sellers and buyers who look for professional high-energy
+        and dedicated expertise that will result in a successful conclusion to their real
+        estate desires or problems.
+        </p>
+        </div>
+    </div>
 </div>
