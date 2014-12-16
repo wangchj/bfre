cb5977328dffd688bc75fba5f4e4343208d29a4e
wangchj <cjw39@hotmail.com>
2014-12-15 21:07:16 -0600

    Change layout.

--- views/site/index.php
+++ views/site/index.php
@@ -25,50 +25,58 @@ HomeAsset::register($this);
     background-color:#565249;">
 
     <div class="container">
-    <div class="row">
-    <div class="col-lg-12">
-    <div class="img-thumbnail">
-    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel" data-interval="10000">
-        <ol class="carousel-indicators" style="bottom:0px;margin-bottom:5px;opacity:0.75">
-            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="2"></li>   
-        </ol>
-        <div class="carousel-inner" role="listbox">
-            <div class="item active" style="overflow:hidden">
-                <img src="http://billfowlerrealestate.com/land_agent_web/images/slider1.jpg" />
-            </div>
-            <div class="item" style="overflow:hidden">
-                <img src="http://billfowlerrealestate.com/land_agent_web/images/slider2.jpg" />
-            </div>
-            <div class="item" style="overflow:hidden">
-                <img src="http://billfowlerrealestate.com/land_agent_web/images/slider3.jpg" />
-            </div>
-        </div>
-    </div>
-    </div>
-    </div>
-    </div>
-    </div>
-</div>
-
-
-<div class="container" style="padding-top:0px">
-    <div class="row">
-        <div class="col-sm-9" style="color: rgb(214, 213, 186);">
-            <h2>Welcome</h2>
-
+        <div class="row">
+            <!-- div class="col-sm-3" style="margin-top:25px">
+                
+            </div -->
+            
+            <div class="col-sm-12" style="color: rgb(214, 213, 186);">
+                
+                <img src="<?=Url::to('@web')?>/images/us.png" align="left" style="margin:25px 30px 10px 10px"/>
+                <h2>Welcome</h2>
+                
                 <p>
-                Bill Fowler Real Estate operates in the Southeaster United Sates and specializes in Land, Commercial, and 1031 Tax-Differed Exchanges. With more than four decades in the business, the company has experienced the full spectrum of issues involved in every aspect of real estate transactions, some of which were extremely complicated.
+                Bill Fowler Real Estate operates in the Southeastern United Sates and specializes in <i>Land</i>, <i>Commercial Properties</i>, and <i>1031 Tax-Differed Exchanges</i>. For almost five decades in business, our company has experienced the full spectrum of issues involved in every aspect of real estate transactions, some of which were extremely complicated.
                 </p>
                 <p>
-                Our company has been successful in transactions that involved numerous entities with diverse goals. Succeeding where other have been unable to craft a deal that satisfies everyone continues to be a hallmark of the company.
+                The services provided have ranged from the sale of a single property to complicated transactions involving the sale and exchange of multiple properties. Our company has been successful in transactions that involved numerous entities with diverse goals. Succeeding where other have been unable to craft a deal that satisfies everyone continues to be a hallmark of the company.
                 </p>
                 <p>
                 We provide personal care with absolute integrity; and as such is the go-to organization for both sellers and buyers who look for professional high-energy and dedicated expertise that will result in a successful conclusion to their real estate desires or problems.
                 </p>
+            </div>
+        </div><!-- row -->
+    </div><!-- container -->
+</div>
 
+
+<div class="container" style="padding-top:25px">
+    <div class="row">
+
+        <div class="col-sm-9" style="margin-top:25px">
+        <div class="img-thumbnail">
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel" data-interval="10000">
+            <ol class="carousel-indicators" style="bottom:0px;margin-bottom:5px;opacity:1">
+                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="2"></li>   
+            </ol>
+            <div class="carousel-inner" role="listbox">
+                <div class="item active" style="overflow:hidden">
+                    <img src="http://billfowlerrealestate.com/land_agent_web/images/slider1.jpg" />
+                </div>
+                <div class="item" style="overflow:hidden">
+                    <img src="http://billfowlerrealestate.com/land_agent_web/images/slider2.jpg" />
+                </div>
+                <div class="item" style="overflow:hidden">
+                    <img src="http://billfowlerrealestate.com/land_agent_web/images/slider3.jpg" />
+                </div>
+            </div>
         </div>
+        </div>
+        </div>
+
+        
 
         <div class="col-sm-3">
 
