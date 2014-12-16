f3ce44ad007f440a25b3a9c514b535ac9650814b
wangchj <cjw39@hotmail.com>
2014-12-15 21:07:44 -0600

    Change layout and message.

--- views/site/about.php
+++ views/site/about.php
@@ -1,5 +1,6 @@
 <?php
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 $this->title = Yii::$app->name . ' About';
@@ -14,41 +15,81 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <div class="row">
         <div class="col-sm-2">
-            <img src="http://billfowlerrealestate.com/images/jdirwin.jpg" class="img-thumbnail" />
+            
         </div>
         <div class="col-sm-8">
-        
-            <p>
-            Bill Fowler Real Estate operates in the Southeaster United Sates and specializes
-            in Land, Commercial, and 1031 Tax-Differed Exchanges. With more than four decades
-            in the business, the company has experienced the full spectrum of issues involved
-            in every aspect of real estate transactions, some of which were extremely complicated.
+            <img src="<?=Url::to('@web')?>/images/Photo.jpg" style="float:left;width:160px;margin:0px 25px 0px 0px;"/>
+            For decades the professionals at BFRE have served a wide variety of clients including individuals, corporations, banks and attorneys with both personal care and absolute integrity. The application of high-energy and dedicated expertise, coupled with a professional attitude that is focused on service, has ensured our clients that, regardless of the complexity of the real estate issue or the difficulty in satisfying diverse goals, an optimum solution to their real estate problems will be efficiency concluded in a timely manner.
             </p>
+<br>
+            <h2>Buyers</h2>
 
-            <p>
-            Our company has been successful in transactions that involved numerous
-            entities with diverse goals. Succeeding where other have been unable to craft a
-            deal that satisfies everyone continues to be a hallmark of the company.
-            </p>
+<p>
+            Buying a property is an exciting time in one’s life. Making the smart move of choosing a REALTOR® is your first step to ensuring that your new home and community meets your needs. My services and experience range from financial aid to helping you find the home that best suits you and your family. I pride myself on repeat business and hope you’ll come to understand why.
+</p>
+<p>
+As Your Agent, I Will:
+</p>
+<p>
+<ul>
+<li>Assure that you see all the properties in the area that meet your criteria.</li>
+<li>Guide you through the entire home buying process, from finding homes to look at, to getting the best financing.</li>
+<li>Make sure you don’t pay too much for your new home and help you avoid costly mistakes.</li>
+<li>Answer all of your questions about the local market area, including schools, neighborhoods, the local economy, and more.</li>
+</ul>
+</p>
+<p>
+Before You Start Looking For Your New Home:
+<ul>
+<li>Check your credit rating. Straighten out any errors before its too late.</li>
+<li>Determine a comfortable monthly budget for your new purchase, including down payment and monthly payment.</li>
+<li>Find a loan that meets your needs and get pre-qualified (preferably pre-approved).</li>
+<li>Choose a REALTOR® that you trust and who understands your needs.</li>
+<li>Determine what neighborhood best matches your needs.</li>
+<li>Identify important features you need your new home to have.</li>
+</ul>
+</p>
+<p>
+Closing Costs to Expect:
+<ul>
+<li>Lender fees include charges for loan processing, underwriting, preparation and establishing an escrow account.</li>
+<li>Third-party fees include charges for insurance, title search, and other inspections such as termites.</li>
+<li>Government fees include deed recording and state &amp; local mortgage taxes.</li>
+<li>Escrow and interest fees include homeowner’s insurance, loan interest, real estate taxes, and occasionally private mortgage insurance.</li>
+</ul>
+</p>
 
-            <p>
-            We provide personal care with absolute integrity; and as such is the go-to
-            organization for both sellers and buyers who look for professional high-energy
-            and dedicated expertise that will result in a successful conclusion to their real
-            estate desires or problems.
-            </p>
-        </div>
-    </div>
+<h2>Sellers</h2>
+<p>
+Selling your property shouldn't be a stressful ordeal. Making the smart move of choosing a REALTOR® is your first step to ensuring that your investment in your home pays off. My services and experience allow you to focus on your move while I manage your home sale from our initial consultation to the closing deal, and beyond. I pride myself on repeat business and hope you’ll come to understand why.
+</p>
 
-    <div class="row">
-        <div class="col-sm-8 col-sm-offset-2">
-            <img style="margin:20px 0px" src="../images/southeast.png" class="img-thumbnail" />
+<p>
+As Your Agent, I Will:
+<ul>
+<li>Complete a comparative market analysis that will compare your home’s value to that of your neighbors.</li>
+<li>Compile a comprehensive plan detailing all the efforts I will employ to sell your home, including Internet and local media.</li>
+<li>Present your home to as many qualified buyers as possible getting your home maximum exposure.</li>
+<li>Help you stage your home and generate curb appeal to ensure you get the highest price.</li>
+<li>Assist with obtaining offers and help you in negotiating the best deal as smoothly as possible.</li>
+<li>Help you find your next home and answer all of your questions about the local market area, including schools, neighborhoods, the local economy, and more.</li>
+</ul>
+</p>
 
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-        
-            <br/>
+<p>
+Recent Home Sales:
+What are homes selling for on your street? Contact me to find out what neighborhood homes are selling for, free of charge, or choose a more detailed analysis of the value of your home. Getting the Highest Price for Your Home
+Curb appeal is key and could make a difference whether people stop and take a flyer, or drive right by. Staging your home is important and many buyers will stay in your home longer if it’s staged appropriately. I have experience, fresh ideas and many local resources to present your home in the most effective manner. Closing Costs to Expect:
+</p>
+<p>
+Title insurance fees depend on the sales price of the home.
+Broker’s commission is a full-service fee and will cost anywhere between 5% to 7%.
+Local property transfer tax, county transfer tax, state transfer tax, and state capital gains tax are the charges that you’ll pay for the privilege of selling your home. Credit to the buyer of unpaid real estate taxes for the prior or current year are variable and depend on when you close and when your taxes are due.
+FHA fees and costs are all fees are now negotiable between an FHA buyer and seller.
+Home inspections fees are in some circumstances paid for by the seller and include pest, radon and other inspections.
+Miscellaneous fees can accrue from correcting problems noticed during the home inspection.
+</p>
 
-            <img style="margin:20px 0px;width:100%" src="../images/2533070092.jpg" class="img-thumbnail" />
         </div>
     </div>
 </div>
