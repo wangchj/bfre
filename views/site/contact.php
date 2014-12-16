cf6bc083370914418d3ccdc9bf737e47cb753f1c
wangchj <cjw39@hotmail.com>
2014-12-15 23:32:24 -0600

    Contact form and mail transport settings.

--- views/site/contact.php
+++ views/site/contact.php
@@ -10,53 +10,47 @@ use yii\captcha\Captcha;
 $this->title = Yii::$app->name . ' Contact';
 ?>
 <div class="container" style="padding-top:0px">
-    <h2>Contact Us</h2>
 
     <div class="row">
-        <div class="col-sm-6">
-        <strong>Sales & Product Inquiries</strong>
-<p>
-Apple Online Store
-The Apple Online Store is a convenient place to purchase Apple products and accessories from Apple and other manufacturers. You can buy online, or by calling (800) MY-APPLE (800-692-7753), 7 days a week from 4:00 a.m. to 10:00 p.m. Pacific time.
-<p>
-You can get information about an order you placed on the Apple Online Store through the Order Status page. If you prefer, you can also get order status or make changes by phone at (800) 692-7753, 7 days a week from 4:00 a.m. to 10:00 p.m. Pacific time.
-<p>
-Apple Retail Stores
-<br>Experience the digital lifestyle at any of the Apple Retail Stores around the country. Find store hours and contact information for all locations.
-<p>
-How to Buy for Business
-<br>If you are a business or professional user, visit the Apple Store for Business or call 1-800-854-3680, 7 days a week from 4:00 a.m. to 10:00 p.m. Pacific time.
-<p>
-Corporate and Government Sales:
-<br>Apple Enterprise Sales
-<br>(877) 412-7753
-<br>Apple Goverment Sales
-<br>(877) 412-7753
-        </div>
+        <div class="col-sm-6 col-sm-offset-3">
+            
+            <h2>Contact Us</h2>
 
-        <div class="col-sm-6">
-        <strong>Product & Technical Support</strong>
-<p>
-Browse online support
-<br>Visit the Apple Support site for quick answers, manuals, and in-depth technical articles. Visit Apple Support Communities to get help and tips from fellow Apple customers.
-<p>
-For help with Beats by Dre headphones and speakers, visit Beats Support. For help with the Beats Music streaming service, visit Beats Music Support.
-<p>
-Contact Apple Support
-<br>Need service or support? Start your request online and we’ll find you a solution.
-<p>
-More ways to get help:
-<br>Contact iTunes Store support
-<br>U.S. iPod, Mac and iPad technical support:
-<br>(800) APL-CARE (800-275-2273)
-<br>U.S. iPhone technical support:
-<br>(800) MY-IPHONE (800-694-7466)
-<br>See all worldwide support telephone numbers
-<br>Contact a mobile carrier
-<br>Make an appointment at an Apple Retail Store Genius Bar
-<br>Find an Apple Authorized Service Provider
-<br>Beats support:
-<br>(800) 442‑4000 (U.S.) or see all worldwide support telephone numbers
+            <?php if (Yii::$app->session->hasFlash('contactFormSubmitted')): ?> 
+                <div class="alert alert-success">
+                Thank you for contacting us. We will respond to you as soon as possible.
+                </div>
+
+                <!-- p>
+                Note that if you turn on the Yii debugger, you should be able
+                to view the mail message on the mail panel of the debugger.
+                <?php if (Yii::$app->mailer->useFileTransport): ?>
+                Because the application is in development mode, the email is not sent but saved as
+                a file under <code><?= Yii::getAlias(Yii::$app->mailer->fileTransportPath) ?></code>.
+                Please configure the <code>useFileTransport</code> property of the <code>mail</code>
+                application component to be false to enable email sending.
+                </p -->
+            <?php endif; ?>
+            
+
+            <?php else: ?>
+
+            <p>
+            If you have business inquiries or other questions, please fill out the following form to contact us. Thank you.
+            </p>
+            <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
+                <?= $form->field($model, 'name') ?>
+                <?= $form->field($model, 'email') ?>
+                <?= $form->field($model, 'subject') ?>
+                <?= $form->field($model, 'body')->textArea(['rows' => 6]) ?>
+                <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
+                    'template' => '<div class="row"><div class="col-sm-3">{image}</div><div class="col-sm-6">{input}</div></div>',
+                ]) ?>
+                <div class="form-group">
+                    <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
+                </div>
+            <?php ActiveForm::end(); ?>
+            <?php endif; ?>
         </div>
     </div>
 </div>
