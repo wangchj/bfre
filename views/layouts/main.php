a1a4f0bc40c08df24bb5ddc76402ca22ab689ac0
wangchj <cjw39@hotmail.com>
2015-05-19 15:07:42 -0500

    Use heading logo.

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -36,12 +36,12 @@ LayoutAsset::register($this);
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="<?=Url::to(['site/index'])?>">
-                        <span style="font-size:1.2em">B</span>ill <span style="font-size:1.2em">F</span>owler <span style="font-size:1.2em">R</span>eal <span style="font-size:1.2em">E</span>state</a>
+                    <a href="<?=Url::to(['site/index'])?>">
+                        <img src="<?=Yii::getAlias('@web/images/heading-logo.png')?>" width="245">
+                    </a>
                 </div>
                 <div id="w0-collapse" class="collapse navbar-collapse">
                     
-                        
                     <ul id="w1" class="navbar-nav navbar-right nav">
                         <li class="active"><a href="<?=Url::to(['site/index'])?>">Home</a></li>
                         <li><a href="<?=Url::to(['site/about'])?>">About</a></li>
