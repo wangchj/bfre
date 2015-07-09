f10de1e4b4a497976d9e4169fa2c8850f351d8f5
wangchj <cjw39@hotmail.com>
2015-07-09 17:01:05 -0500

    Disable map scrolling until user click on the map.

--- views/site/index.js
+++ views/site/index.js
@@ -4,14 +4,34 @@ $(function(){
     })
 });
 
-function initialize() {
-    var mapOptions = {
-        center: { lat: 32.7688, lng: -85.5285},
-        zoom: 6,
-        disableDefaultUI:true
-    };
+var map = {
+    map: null,
+    mapOptions: null,
+    init: function() {
+        this.initOptions();
+        this.map = new google.maps.Map(document.getElementById('map-canvas'), this.mapOptions);
+        this.initEvents();
+    },
+    initOptions: function() {
+        this.mapOptions = {
+            center: { lat: 32.7688, lng: -85.5285},
+            zoom: 6,
+            disableDefaultUI:true,
+            scrollwheel: false
+        };
+    },
+    initEvents: function() {
+        google.maps.event.addListener(this.map, 'click', this.onClick);
+        google.maps.event.addListener(this.map, 'mouseout', this.onMouseout);
+    },
+    onClick: function(e) {
+        //this.mapOptions.scrollwheel = true;
+        this.setOptions({scrollwheel: true});
 
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    },
+    onMouseout: function(e) {
+        this.setOptions({scrollwheel: false});
+    }
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', map.init());
